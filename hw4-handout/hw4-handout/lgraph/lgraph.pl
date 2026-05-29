6d40a05c83641fc13a1c91d0c88a1a146e167990
Haoran Ma <371619924@qq.com>
2026-05-29 16:32:10 +0000

    1

--- hw4-handout/hw4-handout/lgraph/lgraph.pl
+++ hw4-handout/hw4-handout/lgraph/lgraph.pl
@@ -1,3 +1,26 @@
 find_sequence(Graph1, Graph2, Start, Target, K, Sequence) :-
-    %% TODO: remove fail and add body/other cases for this predicate
-    fail.
+    valid_length(Sequence, K),
+    path(Graph1, Start, Target, Sequence),
+    not(path(Graph2, Start, Target, Sequence)).
+
+valid_length(Sequence, K) :-
+    var(K),
+    length(Sequence, K).
+
+valid_length(Sequence, K) :-
+    not(var(K)),
+    K >= 0,
+    length(Sequence, K).
+
+node(Graph, Node) :-
+    edge(Graph, Node, _, _).
+
+node(Graph, Node) :-
+    edge(Graph, _, _, Node).
+
+path(Graph, Node, Node, []) :-
+    node(Graph, Node).
+
+path(Graph, Start, Target, [Label|Rest]) :-
+    edge(Graph, Start, Label, Next),
+    path(Graph, Next, Target, Rest).
